28790bf5c9404938f8341a1a80e0c5fa17f82368
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-01-13 19:32:02 -0300

    Add Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+const Hero = () => (
+  <div>
+    <div>
+      <h1>Adquira conhecimento e se aprimore cada vez mais</h1>
+      <ul>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. N</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. N</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. N</li>
+      </ul>
+      <button>Matricule-se agora</button>
+    </div>
+  </div>
+);
+
+export default Hero;
