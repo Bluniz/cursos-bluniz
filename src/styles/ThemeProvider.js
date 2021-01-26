8a44d94f8fde77cbf768470bee74a76e7cdb86bd
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-01-26 09:00:32 -0300

    Add colors pallte in the global theme

--- src/styles/ThemeProvider.js
+++ src/styles/ThemeProvider.js
@@ -1,7 +1,20 @@
 import { ThemeProvider as StyledProvider } from "styled-components";
 
 const theme = {
-  colorYellow: "#ffc107",
+  colors: {
+    primary: {
+      main: "#ffc107",
+      dark: "#c79100",
+      light: "#fff350",
+      text: "#212121",
+    },
+
+    danger: {
+      main: "#ff3d00",
+      dark: "#b22a00",
+      text: "#212121",
+    },
+  },
 };
 
 const ThemeProvider = ({ children }) => (
