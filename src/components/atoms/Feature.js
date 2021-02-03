1ba5c22eb09699a5f13804bc51d4e1c87eab9506
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-02-03 20:55:33 -0300

    Add more snippets and Start Feature component and Storie

--- src/components/atoms/Feature.js
+++ src/components/atoms/Feature.js
@@ -1,11 +1,23 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Feature = () => (
-  
+const Feature = ({ icon, title, children }) => (
+  <div>
+    <div>{icon}</div>
+    <h5>{title}</h5>
+    <div>{children}</div>
+  </div>
 );
 
-Feature.defaultProps = {};
+Feature.defaultProps = {
+  icon: undefined,
+  title: "",
+  children: undefined,
+};
 
-Feature.propTypes = {};
+Feature.propTypes = {
+  icon: PropTypes.node,
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
 
 export default Feature;
---
+++ src/components/atoms/Feature.stories.js
@@ -0,0 +1,8 @@
+import Feature from "./Feature";
+
+export default {
+  title: "Components/Atoms/Feature",
+  component: Feature,
+};
+
+export const usage = () => <Feature title="Teste">Mais teste</Feature>;
