7dbf9e4241104a87fdef323f5866c8eb45b56192
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-02-07 21:16:16 -0300

    Add Icons, style and storie in Feature component

--- src/components/atoms/Feature.js
+++ src/components/atoms/Feature.js
@@ -1,11 +1,65 @@
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import { breakAt, BreakpointSizes } from "styles/Breakpoints";
+
+const Root = styled.div`
+  text-align: center;
+
+  & h5 {
+    margin: 16px 0;
+    // 1.2rem (font-size) * 1.4 (line-height) * 2 = 3.12rem;
+    min-height: 3.2rem;
+
+    ${breakAt(BreakpointSizes.lg)} {
+      // 1.5rem (font-size) * 1.4 (line-height) * 2 = 3.9rem;
+      height: 3.9rem;
+    }
+  }
+
+  & p {
+    margin: 0;
+  }
+`;
+
+const getPrimaryColor = ({ theme }) => theme.colors.primary.main;
+
+const IconRoot = styled.div`
+  display: inline-block;
+  width: 116px;
+  height: 116px;
+  padding: 8px;
+  border-radius: 50%;
+
+  &:hover {
+    border: 1px solid ${getPrimaryColor};
+    //! > * Pega todos os filhos
+    > * {
+      color: #fff;
+      background-color: ${getPrimaryColor};
+    }
+  }
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #e9e9e9;
+  border-radius: 50%;
+  font-size: 32px;
+  width: 100%;
+  height: 100%;
+  color: ${getPrimaryColor};
+`;
 
 const Feature = ({ icon, title, children }) => (
-  <div>
-    <div>{icon}</div>
+  <Root>
+    <IconRoot>
+      <IconContainer>{icon}</IconContainer>
+    </IconRoot>
     <h5>{title}</h5>
     <div>{children}</div>
-  </div>
+  </Root>
 );
 
 Feature.defaultProps = {
--- src/components/atoms/Feature.stories.js
+++ src/components/atoms/Feature.stories.js
@@ -1,3 +1,10 @@
+import {
+  FaBook,
+  FaSchool,
+  FaChalkboardTeacher,
+  FaMoneyBill,
+} from "react-icons/fa";
+import Grid from "components/atoms/Grid";
 import Feature from "./Feature";
 
 export default {
@@ -5,4 +12,25 @@ export default {
   component: Feature,
 };
 
-export const usage = () => <Feature title="Teste">Mais teste</Feature>;
+export const usage = () => (
+  <Feature title="Teste" icon={<FaBook />}>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </Feature>
+);
+
+export const withGrid = () => (
+  <Grid sm={2} md={4}>
+    <Feature title="Maior e melhor conteudo" icon={<FaBook />}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Feature>
+    <Feature title="Teoria e Prática" icon={<FaSchool />}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Feature>
+    <Feature title="Excelentes professores" icon={<FaChalkboardTeacher />}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Feature>
+    <Feature title="Grátis" icon={<FaMoneyBill />}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Feature>
+  </Grid>
+);
