5dbe594a811a9cea405acd4f9d233dab21c82d21
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-02-19 23:44:16 -0300

    Add Card Component and stories

---
+++ src/components/atoms/Card.js
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+
+import styled from "styled-components";
+
+// #region Card Body
+const StyledBody = styled.div`
+  padding: 16px;
+
+  h6 {
+    margin-top: 0;
+  }
+`;
+
+export const CardBody = ({ children }) => <StyledBody>{children}</StyledBody>;
+
+CardBody.defaultProps = {
+  children: undefined,
+};
+
+CardBody.propTypes = {
+  children: PropTypes.node,
+};
+// #endregion
+
+// #region Card Media
+const StyledMedia = styled.div`
+  background-image: url(${(props) => props.image});
+  background-position: center center;
+  background-size: cover;
+  height: 270px;
+`;
+
+export const CardMedia = ({ image }) => <StyledMedia image={image} />;
+
+CardMedia.defaultProps = {
+  image: undefined,
+};
+
+CardMedia.propTypes = {
+  image: PropTypes.string,
+};
+
+// #endregion
+
+// #region Card
+const Root = styled.div`
+  background: #fff;
+  border-radius: 4px;
+`;
+
+const Card = ({ children }) => <Root>{children}</Root>;
+
+Card.defaultProps = {
+  children: undefined,
+};
+
+Card.propTypes = {
+  children: PropTypes.node,
+};
+// #endregion
+
+export default Card;
