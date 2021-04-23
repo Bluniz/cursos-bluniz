61d5876baef10ed0284ee6368b00bca04ed48a8c
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-04-23 20:41:21 -0300

    Fix Accordion style

---
+++ src/components/atoms/Accordion.stories.js
@@ -0,0 +1,23 @@
+import Accordion from "./Accordion";
+
+export default {
+  title: "Components/Atoms/Accordion",
+  component: Accordion,
+};
+
+export const usage = () => (
+  <>
+    <Accordion title="Como renovo meu bootcamp?">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint sunt
+      voluptas porro recusandae deleniti re
+    </Accordion>
+    <Accordion title="Como faço a mudança de bootcamp?">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint sunt
+      voluptas porro recusandae deleniti re
+    </Accordion>
+    <Accordion title="Não conclui no periodo, e agora?">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint sunt
+      voluptas porro recusandae deleniti re
+    </Accordion>
+  </>
+);
